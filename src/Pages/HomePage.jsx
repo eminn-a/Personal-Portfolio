bc18552240d4a5ced7dd9d0fc12c7983941971c8
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-01-09 17:04:21 +0200

    Added My skills and experties

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../Components/HeroSection/HeroSection";
+import MySkills from "../Components/Skills/MySkills";
 
 export default function HomePage() {
   return (
     <>
-      <HeroSection />;
+      <HeroSection />
+      <MySkills />;
     </>
   );
 }
