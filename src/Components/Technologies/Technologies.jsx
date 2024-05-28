006bcc4126cc2073dddd4004ba63e81a7af84e19
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-05-28 22:23:16 +0300

    Framer motion added

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -10,6 +10,21 @@ import { IoLogoNodejs } from "react-icons/io5";
 import { SiExpress } from "react-icons/si";
 import { SiTypescript } from "react-icons/si";
 
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
 export default function Technologies() {
   return (
     <section className={styles.technologiesSection} id="mySkills">
@@ -18,33 +33,78 @@ export default function Technologies() {
         <h2 className="skills--section--heading">My Technologies</h2>
       </div>
       <div className={styles.iconContainer}>
-        <div className={styles.iconBox}>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <FaHtml5 className={styles.icon} style={{ color: "#e14e1d" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.8)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <FaCss3Alt className={styles.icon} style={{ color: "#0277bd" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <SiJavascript className={styles.icon} style={{ color: "#f7e018" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.3)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <SiTypescript className={styles.icon} style={{ color: "#0277bd" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.7)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <RiReactjsLine className={styles.icon} style={{ color: "#61dafb" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4.2)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <FaAngular className={styles.icon} style={{ color: "#c50836" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4.3)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <IoLogoNodejs className={styles.icon} style={{ color: "#7cb700" }} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.9)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <SiExpress className={styles.icon} />
-        </div>
-        <div className={styles.iconBox}>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.4)}
+          initial="initial"
+          animate="animate"
+          className={styles.iconBox}
+        >
           <SiMongodb className={styles.icon} style={{ color: "green" }} />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
