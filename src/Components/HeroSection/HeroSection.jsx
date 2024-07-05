23efa580dda007fd4cbfe788aeb59401d1bd8bbf
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-07-05 16:27:29 +0300

    Visual fixes

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -13,8 +13,6 @@ export default function HeroSection() {
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Emin</p>
           <h1 className="hero--section--title">
-            {/* <span className="hero--section-title--color">Front-End </span> */}
-
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
--- src/Components/MyProjects/Projects.jsx
+++ src/Components/MyProjects/Projects.jsx
@@ -5,8 +5,8 @@ const Projects = ({ data }) => {
   console.log(data);
   return (
     <div className={styles.trip} id="myPortfolio">
-      <h1>Recent projects</h1>
       <p>My Portfolio</p>
+      <h1>Recent projects</h1>
       <div className={styles.tripCardContainer}>
         {data.map((x, index) => (
           <SingleProject {...x} key={x.id} count={index} />
