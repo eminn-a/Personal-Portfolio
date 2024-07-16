3244f01cf4459310c295de32c9b3069beb439df8
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-07-16 14:39:55 +0300

    About changed

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -12,12 +12,14 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section--description">
-            I'm Emin, a passionate programmer currently following the JavaScript
-            curriculum at SoftUni University, which includes various modules
-            from Basic to more Advanced concepts in the language. Over the past
-            year, I have dedicated myself to honing my technical skills and
-            expanding my knowledge in JavaScript, React, Node.js, MongoDB, and
-            other cutting-edge technologies.
+            I am Emin, a passionate programmer with a comprehensive background
+            in JavaScript, having completed the rigorous curriculum at SoftUni
+            University. Over the past year, I have dedicated myself to honing my
+            technical skills and expanding my knowledge in JavaScript, React,
+            Angular, Node.js, MongoDB, and other cutting-edge technologies. I am
+            currently engaged in the Tech Lead Academy 2 internship, where I am
+            applying my skills to real-world projects and gaining invaluable
+            industry experience.
           </p>
           <p className="hero--section--description">
             I'm not just into coding, I'm also a sports enthusiast! I'm always
